a0a30b55bfab08db909c06cfa4b152608a19b3e0
Kadek Sudiartha <sudieartha@gmail.com>
2024-06-21 17:28:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return 'Hello World from sudigitalCluster X Today in {} is cold!!!'.format(current_date)
+    return 'Mantap!!! Hello World from sudigitalCluster X Today in {} is cold!!!'.format(current_date)
 # main driver function
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)
